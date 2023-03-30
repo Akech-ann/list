35d3e6673690edf9600159f4e1ff463c82e69ccb
Akech-ann <akechannatong@gmail.com>
2023-03-30 17:41:44 +0300

    made changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,19 @@ fun main() {
 println(even(listOf("ann","ivy","book","Grace","pen","pencil","akech","atong","rebecca","maria")))
    println(avgheight(listOf(345.0,675.0,468.0)))
    people()
+    pers()
+    var cars = listOf(
+        Car("ABCD123",120300.0),
+        Car("SHKD76",28369.0),
+        Car("KJDKXK",26891.9),
+        Car("ASJXKDKSK",38392.90)
+
+
+
+    )
+    var avgmileage = calculate(cars)
+    println("The average mileage is :$avgmileage")
+
 }
 fun even(num:List<String>):List<String>{
     return num.filterIndexed { index,num->index%2==0 }
@@ -33,6 +46,15 @@ fun people(){
 }
 //Given a list similar to the one above, write a function in which you will
 //create 2 more people objects and add them to the list at one go.
+data class Persons(var name:String,var age:Int,var height:Double,var weight:Double)
+fun pers(){
+    var per = mutableListOf<Persons>()
+    per.add(Persons("maria",37,56.0,383.0))
+    per.add(Persons("Eshter",12,23.9,38.0))
+println(pers())
+}
+
+
 
 
 
@@ -41,3 +63,11 @@ fun people(){
 //Write a function that takes in a list of Car objects each with a
 //registration and mileage attribute and returns the average mileage of
 //all the vehicles in the list.
+data class Car(var registration:String,var mileage:Double)
+fun calculate(cars:List<Car>):Double{
+    var totalmileage = 0.0
+    for(car in cars){
+        totalmileage+=car.mileage
+    }
+    return totalmileage/cars.size
+}
